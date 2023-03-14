267fa3ff3887626a6b708058bfe4ace81dcd51b6
ali <codewithkiani@gmail.com>
2023-03-14 17:05:39 +0330

    add base menu in main function

---
+++ Go/LinkedList/SinglyLinkedList/singlyLinkedList.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	for {
+		fmt.Println("1. Insert in begin")
+		fmt.Println("2. Insert in last")
+		fmt.Println("3. Insert in position")
+		fmt.Println("4. Delete in position")
+		fmt.Println("5. Display in last")
+		fmt.Println("6. Exit")
+		fmt.Println("Enter a number between 1 and 6:")
+
+		var i int
+		num, _ := fmt.Scanf("%d", &i)
+
+		switch {
+		case num == 1:
+			os.Exit(0)
+		case num == 6:
+			os.Exit(0)
+		}
+	}
+
+}
