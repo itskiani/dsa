3763fe8456e4309991f72d51172c8ace4b2d272f
ali <codewithkiani@gmail.com>
2023-03-20 07:36:50 +0330

    add listMenu and main functions into doublyLinkedList.cpp

--- Cpp/LinkedList/DoublyLinkedList/doublyLinkedList.cpp
+++ Cpp/LinkedList/DoublyLinkedList/doublyLinkedList.cpp
@@ -8,3 +8,17 @@ struct node {
     struct node* prev;
 } *head;
 
+void listMenu(){
+    cout << "1. Insert in begin" << endl;
+    cout << "2. Insert in last" << endl;
+    cout << "3. Insert in position" << endl;
+    cout << "4. Delete in position" << endl;
+    cout << "5. Dispaly linkedlist" << endl;
+    cout << "6. Exit" << endl;
+    cout << "\n Enter a number between 1 and 6: " << endl;
+}
+
+int main(){
+    listMenu();
+    return 0;
+}
