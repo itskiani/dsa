98318277a8cc00e76d84baad5a78fa87e58f565b
ali <codewithkiani@gmail.com>
2023-03-21 10:20:52 +0330

    add listMenu function into doublyLinkedList.go

--- Go/LinkedList/DoublyLinkedList/doublyLinkedList.go
+++ Go/LinkedList/DoublyLinkedList/doublyLinkedList.go
@@ -9,3 +9,15 @@ type Node struct {
 	next *Node
 	prev *Node
 }
+
+func listMenu() {
+	fmt.Println("1. Insert in begin")
+	fmt.Println("2. Insert in last")
+	fmt.Println("3. Insert in position")
+	fmt.Println("4. Delete in position")
+	fmt.Println("5. Display linkedlist")
+	fmt.Println("6. Exit")
+	fmt.Println("Enter a number between 1 and 6:")
+}
+
+func main() {}
