54f56c4b4be4679c8f94c8756d674c03a72f4c7b
ali <codewithkiani@gmail.com>
2023-04-06 22:34:57 +0330

    create listMenu function into stack.go

--- Go/Stack/stack.go
+++ Go/Stack/stack.go
@@ -1,6 +1,18 @@
 package main
 
+import "fmt"
+
 type Node struct {
 	data string
 	next *Node
 }
+
+func listMenu() {
+	fmt.Println("1. Insert in stack")
+	fmt.Println("2. Remove in stack")
+	fmt.Println("3. Display linkedlist")
+	fmt.Println("4. Exit")
+	fmt.Println("Enter a number between 1 and 3:")
+}
+
+func main() {}
