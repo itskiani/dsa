f26e5e9bf919ad12b92891041bb6a21da7c42d8c
ali <codewithkiani@gmail.com>
2023-05-12 08:33:34 +0330

    update main function in stack.go

--- Go/Stack/stack.go
+++ Go/Stack/stack.go
@@ -30,7 +30,7 @@ func push() {
 func listMenu() {
 	fmt.Println("1. Insert in stack")
 	fmt.Println("2. Remove in stack")
-	fmt.Println("3. Display linkedlist")
+	fmt.Println("3. Display stack")
 	fmt.Println("4. Exit")
 	fmt.Println("Enter a number between 1 and 3:")
 }
