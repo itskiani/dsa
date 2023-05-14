9a7f29b23a34f32a1dbce906d9304f6f9736048d
ali <codewithkiani@gmail.com>
2023-05-14 18:08:15 +0330

    add listMenu function into queue.go

--- Go/Queue/queue.go
+++ Go/Queue/queue.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Node struct {
 	data  int
 	front *Node
@@ -14,6 +16,14 @@ func isFull(queue *Node) bool {
 
 func isEmpty(queue *Node) bool {
 	return (queue.size == 0)
+}
+
+func listMenu() {
+	fmt.Println("1. Insert in queue")
+	fmt.Println("2. Remove in queue")
+	fmt.Println("3. Display queue")
+	fmt.Println("4. Exit")
+	fmt.Println("Enter a number between 1 and 4:")
 }
 
 func main() {
