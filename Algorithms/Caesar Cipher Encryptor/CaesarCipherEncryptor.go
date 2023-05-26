bd37aa321f838f68691e1a4e7e4d590ba408780b
ali <codewithkiani@gmail.com>
2023-05-26 15:06:22 +0330

    add main function into caesar cipher file

--- Algorithms/Caesar Cipher Encryptor/CaesarCipherEncryptor.go
+++ Algorithms/Caesar Cipher Encryptor/CaesarCipherEncryptor.go
@@ -1,1 +1,18 @@
 package main
+
+import "fmt"
+
+func main() {
+	var (
+		input string
+		num   int
+	)
+
+	fmt.Print("Enter a string: ")
+	fmt.Scan(&input)
+	fmt.Print("Enter a number: ")
+	fmt.Scan(&num)
+
+	result := caesarCipher(input, num)
+	fmt.Println("\nResult: ", result)
+}
