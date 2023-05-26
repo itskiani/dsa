ce7132c31aa4ec653d4ae376666cf50a171478f3
ali <codewithkiani@gmail.com>
2023-05-26 15:06:54 +0330

    add caesarCipher function

--- Algorithms/Caesar Cipher Encryptor/CaesarCipherEncryptor.go
+++ Algorithms/Caesar Cipher Encryptor/CaesarCipherEncryptor.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+func caesarCipher(str string, num int) string {
+	shift, offset := int32(num%26), int32(26)
+	letters := []int32(str)
+
+	for i, char := range letters {
+		if char >= 'a' && char+shift <= 'z' {
+			char += shift
+		} else {
+			char += shift - offset
+		}
+
+		letters[i] = char
+	}
+
+	return string(letters)
+}
+
 func main() {
 	var (
 		input string
